3c8d48aa07d61dc2bac9589fd2a5c18d0ad51bb8
Raheem Warriach <raw9846@nyu.edu>
2025-02-25 09:30:36 -0500

    Changes to .env and db.js

--- crud-api/config/db.js
+++ crud-api/config/db.js
@@ -1,12 +1,15 @@
 
 const { Pool } = require('pg');
+require('dotenv').config();
+
+console.log('username = ', process.env.DB_USER);
 
 const pool = new Pool({
-    user: 'raw9846',       
-    host: 'localhost',           
-    database: 'strucdb',         
-    password: 'Resteel123!',  
-    port: 5432,                  
+    user: process.env.DB_USER,       // Database user from .env file
+    host: process.env.DB_HOST || 'localhost',  // Host for database (use .env or default to localhost)
+    database: process.env.DB_NAME || 'strucdb',  // Database name from .env or default to 'strucdb'
+    password: process.env.DB_PASSWORD,  // Database password from .env file
+    port: process.env.DB_PORT || 5432,  // Port for PostgreSQL (use .env or default to 5432)
 });
 
 module.exports = pool;
