ddc1c88dca33fcc020ed98a1097e03cadf475c6a
zack781 <thean781@gmail.com>
2025-04-28 15:25:16 -0400

    fixed image sending pipeline

--- cv/image-taking.js
+++ cv/image-taking.js
@@ -1,15 +1,18 @@
 const corelink = require('./corelink.lib.js')
+const sharp = require('sharp');
 
 const config = {
   ControlPort: 20012,
   // ControlIP: '127.0.0.1',
   ControlIP: process.env.CORELINK_HOST,
+  // ControlIP: '128.110.217.55',
 
   /*
   autoReconnect: false,
     for service in a local network please replace the certificate with the appropriate version
   cert: '<corelink-tools-repo>/config/ca-crt.pem'
   */
+  // cert: '/Users/zack/git_repos/ReSteel/ca-crt.pem'
   cert: process.env.CERT_PATH
 }
 
@@ -20,26 +23,31 @@ const workspace = 'Holodeck'
 const protocol = 'tcp'
 const datatype = 'image-capturing'
 
-process.on('SIGINT', () => {
-  console.log('Disconnect Corelink gracefully...');
-  corelink.disconnect();
-  process.exit(0);
-});
 
-process.on('SIGTSTP', () => {
-  console.log('Disconnect Corelink gracefully...');
-  corelink.disconnect();
-  process.exit(0);
-});
 
 const fs = require('fs');
 const { spawn } = require('child_process');
 
 let imgCount = 0;
+let startLoop = false;
+let sender =null;
+
+process.on('SIGINT', () => {
+  console.log('Disconnect Corelink gracefully...');
+  corelink.disconnect({workspaces: [workspace], types: [datatype]});
+  process.exit(0);
+});
+
+process.on('SIGTSTP', () => {
+  console.log('Disconnect Corelink gracefully...');
+  corelink.disconnect({workspaces: [workspace], types: [datatype]});
+  process.exit(0);
+});
+
 const run = async () => {
-    // corelink.setDebug(true);
+    corelink.setDebug(true);
     if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
-    let sender = await corelink.createSender({
+    sender = await corelink.createSender({
       workspace,
       protocol,
       type: datatype,
@@ -48,41 +56,8 @@ const run = async () => {
 
     corelink.on('sender', (data) => {
       console.log("sender = ", data);
+      startLoop = true;
 
-      async function loop() {
-        while (true) {
-          // your logic here (preferably non-blocking)
-          const filePath = './test' + imgCount + '.jpg';
-          if (fs.existsSync(filePath)) {
-            console.log('Sending image...');
-            const imgBuff = fs.readFileSync(filePath);
-            const buffer = Buffer.from(imgBuff);
-            console.log('Buffer length:', buffer.length);
-            const bufferLength = buffer.length;
-
-            let counter = 0;
-
-            async function sendChunk() {
-              while (counter < bufferLength) {
-                const chunk = counter + 1024 < bufferLength ? buffer.slice(counter, counter + 1024) : buffer.slice(counter, bufferLength);
-                const lastChunk = counter + 1024 >= bufferLength;
-                console.log('Sending chunk:', counter, 'Last chunk:', lastChunk);
-                corelink.send(sender, chunk, { "seq-num": counter, "last-chunk": lastChunk, "filename": filePath, "file-size": bufferLength });
-                counter += 1024;
-
-                await new Promise(resolve => setTimeout(resolve, 10)); // simulate async wait
-              }
-            }
-
-            sendChunk();
-            imgCount++;
-          }
-          await new Promise(resolve => setTimeout(resolve, 100)); // simulate async wait
-        }
-        console.log('Exited loop');
-      }
-
-      loop();
     })
 
   }
@@ -90,4 +65,55 @@ const run = async () => {
 
 run()
 
+async function loop() {
+  setInterval(async () => {
+    if (startLoop) {
+      const filePath = './test' + imgCount + '.jpg';
+      const outputPath = './output' + imgCount + '.jpg';
+      if (fs.existsSync(filePath)) {
+        if (!fs.existsSync(outputPath)) {
+          sharp(filePath)
+          .jpeg({ quality: 100 })           // Compress JPEG (quality 0-100)
+          .toFile(outputPath)            // Save compressed image
+          .then(() => {
+            console.log('Image compressed and saved!');
+          })
+          .catch(err => {
+            console.error('Compression failed:', err);
+          });
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        }
 
+        console.log('Sending image...');
+        const imgBuff = fs.readFileSync(outputPath);
+        const buffer = Buffer.from(imgBuff);
+        console.log('Buffer length:', buffer.length);
+        const bufferLength = buffer.length;
+
+        let counter = 0;
+        let index = 0;
+
+        async function sendChunk() {
+          while (counter < bufferLength) {
+            startLoop = false;
+            console.log('Sending chunk:', counter);
+            const chunk = counter + 1024 < bufferLength ? buffer.slice(counter, counter + 1024) : buffer.slice(counter, bufferLength);
+            const lastChunk = counter + 1024 >= bufferLength;
+            corelink.send(sender, chunk, { "seq-num": counter, "last-chunk": lastChunk, "filename": outputPath, "file-size": bufferLength, "index": index });
+
+            await new Promise(resolve => setTimeout(resolve, 150));
+
+            counter += 1024;
+            index+=1;
+          }
+          startLoop = true;
+        }
+
+        await sendChunk();
+        imgCount++;
+      }
+    }
+  }, 100); // every 100 ms check
+}
+
+loop()
--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const config = {
   ControlPort: 20012,
   // ControlIP: '127.0.0.1',
   ControlIP: process.env.CORELINK_HOST,
-  // ControlIP: '198.22.255.16',
+  // ControlIP: '128.110.217.55',
   autoReconnect: false,
   /*
     for service in a local network please replace the certificate with the appropriate version
   cert: '<corelink-tools-repo>/config/ca-crt.pem'
   */
   cert: process.env.CERT_PATH
+  // cert: '/Users/zack/git_repos/ReSteel/ca-crt.pem'
 }
 
 const username = process.env.CORELINK_USERNAME
@@ -23,16 +24,25 @@ const protocol = 'tcp'
 const datatype = 'image-capturing'
 var arr = [];
 var index = 0;
+let receiver = null;
 
 process.on('SIGINT', () => {
   console.log('Disconnect Corelink gracefully...');
-  corelink.disconnect();
+  corelink.disconnect({workspaces: [workspace], types: [datatype]});
+  process.exit(0);
+});
+
+process.on('SIGTSTP', () => {
+  console.log('Disconnect Corelink gracefully...');
+  corelink.disconnect({workspaces: [workspace], types: [datatype]});
+
   process.exit(0);
 });
 
 const run = async () => {
+  corelink.setDebug(true);
   if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
-    await corelink.createReceiver({
+    receiver = await corelink.createReceiver({
       workspace,
       protocol,
       type: datatype,
@@ -49,25 +59,50 @@ const run = async () => {
     corelink.on('data', (streamID, data, header) => {
       // console.log(streamID, data.toString(), JSON.stringify(header))
       // console.log('header = ', header);
-      console.log('header[seq-num] = ', header['seq-num']);
+      // console.log('header[seq-num] = ', header['seq-num']);
+      // console.log('data = ', data);
       // if (header['seq-num'] === index) {
       //   arr(data);
       //   index+=2048;
       // }
-      arr[header['seq-num']] = data;
+      console.log('seq-num, index = ', header['seq-num'], ' - ', index);
+      arr[header['index']] = data;
+      index+=1024;
+
 
       if (header['last-chunk']) {
-        console.log('last chunk');
-        const totalChunks = Object.keys(arr).map(Number).sort((a, b) => a - b);
-        const buffers = totalChunks.map(k => {
-          const chunk = arr[k];
-          if (!chunk) {
-            throw new Error(`Missing chunk at sequence ${k}`);
+
+        console.log('arr = ', arr);
+        // console.log('last chunk');
+        // arr = arr.map((chunk, i) => {
+        //   if (!chunk) {
+        //     return Buffer.alloc(1024);
+        //   } else {
+        //     return chunk;
+        //   }
+        // });
+        for (let i = 0; i < Math.ceil(header['file-size'] / 1024); i++) {
+          if (arr[i] === undefined) {
+            arr[i] = Buffer.alloc(1024);
           }
-          return chunk;
-        });
-        const buf = Buffer.concat(buffers);
+        }
+        // const totalChunks = Object.keys(arr).map(Number).sort((a, b) => a - b);
+        // console.log('totalChunks = ', totalChunks);
+        // const buffers = totalChunks.map(k => {
+        //   const chunk = arr[k];
+        //   if (!chunk) {
+        //     console.log('chunk is empty');
+        //     return Buffer.alloc(1024);
+        //   }
+        //   return chunk;
+        // });
+
+        // console.log('buffers = ', buffers);
+
+        console.log('arr = ', arr);
+        const buf = Buffer.concat(arr);
         // console.log('buf = ', buf);
+        console.log(buf.size);
 
         if (!fs.existsSync(header['filename'])) {
           fs.writeFile(header['filename'], buf, function(err) {
